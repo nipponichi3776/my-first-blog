13638fdcd9b08bee0794bbb6a4032ced21d0290a
MT4ver2-x87-xu2FtW6z <MT4ver2-x87-xu2FtW6z@dc.srv>
2021-11-28 14:51:04 +0900

    Modify config

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-flwbyzo=(zj!uys01vmvrl9cu*hv3$$q9bb*2@mzxut2cb=@q='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
